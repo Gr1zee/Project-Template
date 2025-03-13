edd4143e0b76c8aa834d07834bc7958a76ef452b
VladG <gricenkovlad007@gmail.com>
2025-03-13 11:33:47 +0300

    Create Base Template without db and tests

---
+++ app/routers/router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from ..init import templates
+
+
+router = APIRouter()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "name": "World"})
+
+@router.get("/{name}", response_class=HTMLResponse)
+async def read_item(request: Request, name: str):
+    return templates.TemplateResponse("index.html", {"request": request, "name": name})
