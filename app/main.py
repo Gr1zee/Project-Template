49f13776b1b0a31eb368acab02815230f6d07690
VladG <gricenkovlad007@gmail.com>
2025-03-13 21:47:28 +0300

    HUINA NEDODELANYA

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from .routers.router import router
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 app = FastAPI()
 
--- app/routers/router.py
+++ app/routers/router.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from ..init import templates
-
+from ..lib.schemas.Item import Item, Base
+from ..lib.models.Item import ItemModel
+from app.lib.models.Item import SessionLocal
+from sqlalchemy import engine
 
 router = APIRouter()
 
+Base.metadata.create_all(bind=engine)
 
 @router.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -13,3 +17,21 @@ async def read_root(request: Request):
 @router.get("/{name}", response_class=HTMLResponse)
 async def read_item(request: Request, name: str):
     return templates.TemplateResponse("index.html", {"request": request, "name": name})
+
+@router.post("/items/")
+async def create_item(item: Item):
+    db_item = ItemModel(**item.dict())
+    db = SessionLocal()
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    db.close()
+    return db_item
+
+# Эндпоинт для получения всех элементов
+@router.get("/items/")
+async def get_items():
+    db = SessionLocal()
+    items = db.query(ItemModel).all()
+    db.close()
+    return items
