a630a66189a83bff485a09f6d2bb46223442d03f
Arthur <baginian.artur@yandex.ru>
2025-03-16 00:03:04 +0300

    add dark material qt theme

--- client/main.py
+++ client/main.py
@@ -2,6 +2,7 @@ import logging
 import sys
 
 from PyQt6.QtWidgets import QApplication
+from qt_material import apply_stylesheet
 from settings import configure_logging, except_hook
 
 from app.main_window import MainWindow
@@ -11,6 +12,8 @@ def main():
     configure_logging(level=logging.DEBUG)
 
     app = QApplication(sys.argv)
+    # See https://pypi.org/project/qt-material/
+    apply_stylesheet(app, theme="dark_teal.xml")
     main_window = MainWindow()
 
     logging.info("App started")
